dab3ae2c0f7ab4cd3b477c3ffa53bf097297d9f2
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-03 16:19:57 -0300

    feat: criar componente header

---
+++ guardian_bank/src/Component/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Component/Header";
+import "./index.css";
+
+ReactDOM.render(
+<Header title="Lead Collector Login" />,
+  document.getElementById("root")
+);
