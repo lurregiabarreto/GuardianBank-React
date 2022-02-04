b1688386fafbb4c96227ec081b8f0b2fc9ff7eec
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-02-04 08:50:27 -0300

    feat:Criar dashboard

---
+++ guardian_bank/src/Component/pages/Dashboard/index.js
@@ -0,0 +1,15 @@
+import react, {Component} from "react";
+
+class Dashboard extends Component {
+
+    render(){
+        return(
+            <div>
+                <h1>DASHBOARD</h1>
+                <h2>Você foi logado com sucesso. </h2>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
